eec090fa98f9de5c7022846c6d43dd498c95a8a6
‘MR’ <madhu.rebbana@gmail.com>
2019-01-07 23:47:46 -0800

    client server setup

--- notes-client/src/App.js
+++ notes-client/src/App.js
@@ -1,28 +1,80 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment,Component } from "react";
+import {Auth} from 'aws-amplify'
+import { Link,withRouter } from "react-router-dom";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import "./App.css";
+import Routes from "./Routes";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+  constructor(props) {
+  super(props);
+
+ this.state = {
+  isAuthenticated: false,
+  isAuthenticating: true
+};
+  
+}
+async componentDidMount() {
+  try {
+    await Auth.currentSession();
+    this.userHasAuthenticated(true);
   }
+  catch(e) {
+    if (e !== 'No current user') {
+      alert(e);
+    }
+  }
+
+  this.setState({ isAuthenticating: false });
 }
 
-export default App;
+handleLogout = async event => {
+  await Auth.signOut();
+  this.userHasAuthenticated(false);
+  this.props.history.push("/login");
+}
+
+userHasAuthenticated = authenticated => {
+  this.setState({ isAuthenticated: authenticated });
+}
+
+  render() {
+  const childProps = {
+  isAuthenticated: this.state.isAuthenticated,
+  userHasAuthenticated: this.userHasAuthenticated
+};
+    return (
+<div className="App container">
+      <Navbar fluid collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">Scratch</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+{this.state.isAuthenticated
+  ? <NavItem onClick={this.handleLogout}>Logout</NavItem>
+  : <Fragment>
+      <LinkContainer to="/signup">
+        <NavItem>Signup</NavItem>
+      </LinkContainer>
+      <LinkContainer to="/login">
+        <NavItem>Login</NavItem>
+      </LinkContainer>
+    </Fragment>
+}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Routes childProps={childProps} />
+    </div>
+    );
+  }
+   
+}
+
+export default withRouter(App);
